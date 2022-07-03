cc831df7f36bb09eee15f29bcf51631ca9e1a630
Osman <hassan.osman@divine-it.net>
2022-07-03 22:28:44 +0600

    init core

---
+++ src/main/java/com/se/dhuye_daw/DhuyeDawApplication.java
@@ -0,0 +1,13 @@
+package com.se.dhuye_daw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DhuyeDawApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DhuyeDawApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/se/dhuye_daw/DhuyeDawApplicationTests.java
@@ -0,0 +1,13 @@
+package com.se.dhuye_daw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DhuyeDawApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
